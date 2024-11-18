4682388c63481d3a2e17ebfd886c3f6faaa645b9
github_shripad99 <shripadtirlotkar15@gmail.com>
2024-11-18 13:06:07 +0530

    Changes commit

--- src/Components/MovieModal.jsx
+++ src/Components/MovieModal.jsx
@@ -26,14 +26,14 @@ const MovieModal = ({movie, onClose}) => {
     }
   return (
     <div className="modal fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="modal-content bg-[#2a385d] p-6 rounded-lg relative w-3/4 md:w-1/2 lg:w-1/3">
+      <div className="modal-content bg-[#2a385d] p-10 rounded-lg relative w-3/4 md:w-1/2 lg:w-1/3 max-h-screen">
         <button className="close-btn absolute top-2 right-2 text-gray-500" onClick={onClose}>
           <RxCross2 size={24} />
         </button>
-        <h2 className="text-xl font-bold mb-4 text-yellow-400">{movieDetails.title}</h2>
-        <p className='text-gray-500'><strong className='text-white'>Genre:</strong> {movieDetails.genres.map((genre) => genre.name).join(", ")}</p>
-        <p className='text-gray-500'><strong className='text-white'>Summary:</strong> {movieDetails.overview}</p>
-        <p className='text-gray-500'><strong className='text-white'>Rating:</strong> {movieDetails.vote_average}/10</p>
+        <h2 className="text-xl font-semibold mb-4 text-white">{movieDetails.title}</h2>
+        <p className='text-gray-500'><strong className='text-white font-normal'>Genre:</strong> {movieDetails.genres.map((genre) => genre.name).join(", ")}</p>
+        <p className='text-gray-500 py-1'><strong className='text-white font-normal'>Summary:</strong> {movieDetails.overview}</p>
+        <p className='text-gray-500 py-1'><strong className='text-white font-normal'>Rating:</strong> {movieDetails.vote_average}/10</p>
       </div>
     </div>
   )
